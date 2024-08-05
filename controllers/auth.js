3f1b374c589fb25fd654655eeed547540b1e5447
liansun96 <toetetaung3296@gmail.com>
2024-08-06 00:04:09 +0630

    update UserSchema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const bcrypt = require('bcryptjs')
+
 
 const register = async (req , res) => {
     const {name , email , password} = req.body
     console.log(req.body);
-    const salt = await bcrypt.genSalt(10)
-    console.log(salt);
-    const hashedPassword = await bcrypt.hash(password , salt)
-    console.log(hashedPassword);
+    // const salt = await bcrypt.genSalt(10)
+    // console.log(salt);
+    // const hashedPassword = await bcrypt.hash(password , salt)
+    // console.log(hashedPassword);
     const user = await User.create({name ,email , password : hashedPassword})
     res.status(StatusCodes.CREATED).json({user})
 }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -14,13 +15,20 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide valid email",
     ],
-    unique : true
+    unique: true,
   },
-  password : {
-    type : String,
-    require : [true , 'Please provide pasword'],
-    minLength : 3
-  }
+  password: {
+    type: String,
+    require: [true, "Please provide pasword"],
+    minLength: 3,
+  },
 });
 
-module.exports = mongoose.model('User' , UserSchema)
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  console.log(salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  console.log(hashedPassword);
+});
+
+module.exports = mongoose.model("User", UserSchema);
