92dadbab1c2b212aabc5ee00abc43cbf36d61d10
liansun96 <toetetaung3296@gmail.com>
2024-08-18 10:35:05 +0630

    update mongoose error validation to errorHandlerMiddleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,11 +2,24 @@ const { StatusCodes } = require("http-status-codes")
 const { CustomAPIError } = require("../errors")
 
 const errorHandlerMiddleware = (err , req ,res , next) => {
+    console.log(err);
+
+    let customError = {
+        statusCode : err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg : err.message || 'Something went wrong , try again later'
+    }
+    
     if(err instanceof CustomAPIError){
         res.status(err.statusCode).json({msg : err.message})
     }
 
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    if(err.code || err.code === '11000'){
+        customError.statusCode = 400 , 
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field , please choose another value`
+    }
+
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    return res.status(customError.statusCode).json({msg : customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
