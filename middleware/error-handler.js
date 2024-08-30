0c7df7f94e9e6e3cebee24815b8902bf3e0a7a09
liansun96 <liansun0203@gmail.com>
2024-08-30 15:34:39 +0630

    update error handler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,24 +6,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later",
   };
 
-  // if (err instanceof CustomAPIError){
-  //     return res.status(err.statusCode).json({msg : err.message})
-  // }
-
-<<<<<<< HEAD
-  if(err.name === 'ValidatonError'){
-    customError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+ 
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
     customError.statusCode = 400;
   }
-  
-  
-=======
-
-    if(err.code || err.code === '11000'){
-        customError.statusCode = 400 , 
-        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field , please choose another value`
-    }
->>>>>>> 41b7f5bd092fe71a84fe0d1ebe4b2b97c1c7c009
 
   if (err.name === "CastError"){
     customError.msg = `No item found with id : ${err.value}`
@@ -37,7 +26,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
-//   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
